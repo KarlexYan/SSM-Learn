90b2f66501992ef274f5b319a0d6ad76d6ee16a4
KarlexYan <KarlexYan@aliyun.com>
2023-02-09 21:37:07 +0800

    Spring DI入门案例

--- Spring-QuickStart/src/main/java/com/KarlexYan/service/impl/BookServiceImpl.java
+++ Spring-QuickStart/src/main/java/com/KarlexYan/service/impl/BookServiceImpl.java
@@ -1,16 +1,20 @@
 package com.KarlexYan.service.impl;
 
 import com.KarlexYan.dao.BookDao;
-import com.KarlexYan.dao.impl.BookDaoImpl;
 import com.KarlexYan.service.BookService;
 
 public class BookServiceImpl implements BookService {
-    private BookDao bookDao = new BookDaoImpl();
-
-
+//    private BookDao bookDao = new BookDaoImpl();
+//    5.删除业务层中使用new方式创建的dao对象
+        private BookDao bookDao;
     @Override
     public void save() {
         System.out.println("book service save ...");
         bookDao.save();
+    }
+
+//    6.提供对应的setter方法
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
     }
 }
