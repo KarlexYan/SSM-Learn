276e9eefe80943106540ae19a6fdf8ea19a6d6cc
KarlexYan <KarlexYan@aliyun.com>
2023-02-24 22:41:40 +0800

    SpringMVC get请求与post请求普通参数传递 解决post中文乱码

---
+++ SpringMVC/Request-Param/src/main/java/com/KarlexYan/config/SpringMvcConfig.java
@@ -0,0 +1,9 @@
+package com.KarlexYan.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.KarlexYan.controller")
+public class SpringMvcConfig {
+}
