253a730a9b9298bc65fe8151584b7f3a6050fb1f
KarlexYan <KarlexYan@aliyun.com>
2023-03-06 17:41:49 +0800

    SpringBoot 配置文件 properties yml yaml

---
+++ SpringBoot/SpringBoot-BaseConfig/src/main/java/com/karlexyan/SpringBootBaseConfigApplication.java
@@ -0,0 +1,13 @@
+package com.karlexyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBaseConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootBaseConfigApplication.class, args);
+    }
+
+}
