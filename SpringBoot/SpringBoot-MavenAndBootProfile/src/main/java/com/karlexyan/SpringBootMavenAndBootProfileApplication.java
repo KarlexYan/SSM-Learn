2426dbafb9b8a72db88b5137f12c3fffc95ed619
KarlexYan <KarlexYan@aliyun.com>
2023-03-07 11:50:28 +0800

    SpringBoot 多环境开发兼容问题（Maven和SpringBoot）

---
+++ SpringBoot/SpringBoot-MavenAndBootProfile/src/main/java/com/karlexyan/SpringBootMavenAndBootProfileApplication.java
@@ -0,0 +1,13 @@
+package com.karlexyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMavenAndBootProfileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootMavenAndBootProfileApplication.class, args);
+    }
+
+}
