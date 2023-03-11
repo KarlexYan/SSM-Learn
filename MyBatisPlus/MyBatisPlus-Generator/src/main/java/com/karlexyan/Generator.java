a16d9ab37b2495850c4d3f69557df0a82d3674e5
KarlexYan <KarlexYan@aliyun.com>
2023-03-11 21:50:38 +0800

    MybatisPlus 代码生成器

---
+++ MyBatisPlus/MyBatisPlus-Generator/src/main/java/com/karlexyan/Generator.java
@@ -0,0 +1,19 @@
+package com.karlexyan;
+
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.config.DataSourceConfig;
+
+public class Generator {
+    public static void main(String[] args) {
+        AutoGenerator autoGenerator = new AutoGenerator();
+        DataSourceConfig dataSource = new DataSourceConfig();
+
+        dataSource.setDriverName("com.mysql.cj.jdbc.Driver");
+        dataSource.setUrl("jdbc:mysql://localhost:3306/db");
+        dataSource.setUsername("root");
+        dataSource.setPassword("root");
+        autoGenerator.setDataSource(dataSource);
+
+        autoGenerator.execute();
+    }
+}
---
+++ MyBatisPlus/MyBatisPlus-Generator/src/main/java/com/karlexyan/MyBatisPlusGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.karlexyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBatisPlusGeneratorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBatisPlusGeneratorApplication.class, args);
+    }
+
+}
